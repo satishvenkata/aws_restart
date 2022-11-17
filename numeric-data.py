d118e23a0d57585a2455f14d2fb0fca90532ef21
satishvenkata <118172635+satishvenkata@users.noreply.github.com>
2022-11-17 15:30:24 +0530

    master

---
+++ numeric-data.py
@@ -0,0 +1,5 @@
+print("Python has three numeric types: int, float, and complex")
+myvalue = 1
+print(myvalue)
+print(type(myvalue))
+print(str(myvalue) + " is of the data type " + str(type(myvalue)))
